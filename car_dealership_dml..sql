e349c96fb694ecae7add47e2ea00bd0f64e5509b
ZCosco <zachariahcos@gmail.com>
2023-02-24 17:55:42 -0800

    changes

---
+++ car_dealership_dml..sql
@@ -0,0 +1,23 @@
+INSERT INTO Customer(
+    first_name,
+    last_name,
+    customer_address,
+    phone_num
+) VALUES (
+    'christian',
+    'askew',
+    '555 thieves st',
+    '555-555-5555'
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    customer_address,
+    phone_num
+) VALUES (
+    'John',
+    'Doe',
+    '555 thieves st',
+    '555-555-5555'
+);
