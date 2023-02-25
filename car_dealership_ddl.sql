9f344303d311b9f742c1efdf0f8c1077cdd3fce6
Cameron Lee <camdlee18@gmail.com>
2023-02-25 15:32:30 -0500

    first commit

--- car_dealership_ddl.sql
+++ car_dealership_ddl.sql
@@ -1,59 +1,86 @@
-CREATE TABLE "salesperson" (
-  "salesperson_id" SERIAL,
-  "first_name" VARCHAR(50),
-  "last_name" VARCHAR(50),
-  PRIMARY KEY ("salesperson_id")
+CREATE TABLE salesperson (
+  salesperson_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
 );
 
-CREATE TABLE "invoice" (
-  "invoice_id" SERIAL,
-  "salesperson_id" INTEGER,
-  "customer_id" INTEGER,
-  "car_id" INTEGER,
-  "invoice_date" SMALLDATETIME,
-  "invoice_amount" INTEGER,
-  PRIMARY KEY ("invoice_id")
+CREATE TABLE Customer (
+  customer_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  address VARCHAR(100),
+  phone_num VARCHAR(15)
 );
 
-CREATE TABLE "Customer" (
-  "customer_id" SERIAL,
-  "first_name" VARCHAR(50),
-  "last_name" VARCHAR(50),
-  "address" VARCHAR(100),
-  "phone_num" VARCHAR(50),
-  "service_ticket_id" INTEGER,
-  PRIMARY KEY ("customer_id")
+CREATE TABLE invoice (
+  invoice_id SERIAL PRIMARY KEY,
+  salesperson_id INTEGER,
+  FOREIGN KEY (salesperson_id) REFERENCES salesperson(salesperson_id),
+  customer_id INTEGER,
+  FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+  car_id INTEGER,
+  FOREIGN KEY (car_id) REFERENCES car(car_id),
+  invoice_date TIMESTAMP,
+  invoice_amount INTEGER
 );
 
-CREATE TABLE "Car" (
-  "car_id" SERIAL,
-  "vehicle_id_num" VARCHAR(50),
-  "service_ticket" INTEGER,
-  "make" VARCHAR,
-  "model" VARCHAR,
-  "year" INTEGER,
-  PRIMARY KEY ("car_id")
+CREATE TABLE Car (
+  car_id SERIAL PRIMARY KEY,
+  vehicle_id_num VARCHAR(50),
+  car_make VARCHAR(50),
+  car_model VARCHAR(50),
+  car_year INTEGER
 );
 
-CREATE TABLE "service_ticket" (
-  "service_ticket_id" SERIAL,
-  "car_id" INTEGER,
-  "service_ticket_amount" VARCHAR(50),
-  "services_rendered" VARCHAR(100),
-  PRIMARY KEY ("service_ticket_id")
+CREATE TABLE service_ticket (
+  service_ticket_id SERIAL PRIMARY KEY,
+  customer_id INTEGER,
+  FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+  car_id INTEGER,
+  FOREIGN KEY (car_id) REFERENCES car(car_id),
+  service_ticket_amount VARCHAR(50),
+  services_rendered VARCHAR(100),
+  service_time TIMESTAMP
 );
 
-CREATE TABLE "service_history" (
-  "service_history_id" SERIAL,
-  "mechanic_id" INTEGER,
-  "service_performed" VARCHAR(50),
-  "service_ticket_id" INTEGER,
-  PRIMARY KEY ("service_history_id")
+CREATE TABLE service_history (
+  service_history_id SERIAL PRIMARY KEY,
+  mechanic_id INTEGER,
+  FOREIGN KEY (mechanic_id) REFERENCES mechanic(mechanic_id),
+  service_performed VARCHAR(50),
+  service_ticket_id INTEGER,
+  FOREIGN KEY (service_ticket_id) REFERENCES service_ticket(service_ticket_id)
 );
 
-CREATE TABLE "mechanic" (
-  "mechanic_id" SERIAL,
-  "first_name" VARCHAR(50),
-  "last_name" VARCHAR(50),
-  PRIMARY KEY ("mechanic_id")
+CREATE TABLE mechanic (
+  mechanic_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
 );
+
+
+
+----------- CHANGES TO TABLES -------------
+
+ALTER TABLE customer
+RENAME COLUMN address to customer_address;
+
+ALTER TABLE car
+DROP vehicle_id_num;
+
+-- CHANGED TO DECIMAL(19,4) FOR PRECISION
+ALTER TABLE invoice
+ALTER COLUMN invoice_amount TYPE DECIMAL(19,4);
+
+-- ALTER TABLE service_ticket
+-- ALTER COLUMN service_ticket_amount TYPE DECIMAL(19,4);
+-- ABOVE WOULDN'T WORK PROPERLY SO I DID BELOW
+ALTER TABLE service_ticket
+DROP service_ticket_amount;
+
+ALTER TABLE service_ticket
+ADD service_ticket_amount DECIMAL(19,4);
+
+ALTER TABLE service_history
+DROP service_performed;
+---since ticket already has services rendered
--- car_dealership_dml..sql
+++ car_dealership_dml..sql
@@ -4,42 +4,128 @@ INSERT INTO Customer(
     customer_address,
     phone_num
 ) VALUES (
-    'christian',
-    'askew',
-    '555 thieves st',
+    'dylan',
+    'katina',
+    '1234 coding temple dr',
     '555-555-5555'
-);
+),
 (
-    'Lambo',
-    'Ferrari',
-    '553 thieves st',
-    '555-555-5555'
+    'kim',
+    'kardashian',
+    '1235 coding temple dr',
+    '555-555-5556'
 );
-(
-    'Yosemite',
-    'tsuchida',
-    '553 thieves st',
-    '555-555-5555'
-);
-(
-    'eric',
-    'Oshounosy',
-    '553 thieves st',
-    '555-555-5555'
-);
+
+SELECT *
+FROM customer;
 
 INSERT INTO salesperson(
     first_name,
-    last_name,
+    last_name
 ) VALUES (
-    'John',
-    'Doe',
-);
+    'christian',
+    'askew'
+),
 (
-    'Mike',
-    'Smith',
+    'kris',
+    'jenner'
 );
+
+SELECT *
+FROM salesperson;
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year
+) VALUES (
+    'honda',
+    'civc',
+    '2022'
+),
 (
-    'Thanos',
-    'Carter',
+    'tesla',
+    'y',
+    '2021'
 );
+
+SELECT *
+FROM car;
+
+INSERT INTO invoice (
+    salesperson_id,
+    customer_id,
+    car_id,
+    invoice_date,
+    invoice_amount
+) VALUES (
+    '1',
+    '1',
+    '1',
+    CURRENT_TIMESTAMP,
+    50000
+),
+(
+    '2',
+    '2',
+    '2',
+    CURRENT_TIMESTAMP,
+    80000
+);
+
+SELECT *
+FROM invoice;
+
+INSERT INTO mechanic (
+    first_name,
+    last_name
+) VALUES (
+    'devin',
+    'volk'
+),
+(
+    'anifa',
+    'chishungu'
+);
+
+SELECT *
+FROM mechanic;
+
+INSERT INTO service_ticket (
+    customer_id,
+    car_id,
+    service_ticket_amount,
+    services_rendered,
+    service_time
+) VALUES (
+    '1',
+    '1',
+    700,
+    'changed tires, oil, wiper fluid, fixed windshield',
+    CURRENT_TIMESTAMP
+),
+(
+    '2',
+    '2',
+    1000,
+    'fixed the headlights',
+    CURRENT_TIMESTAMP
+);
+
+SELECT *
+FROM service_ticket;
+
+INSERT INTO service_history (
+    mechanic_id,
+    service_ticket_id
+) VALUES (
+    '1',
+    '1'
+),
+(
+    '2',
+    '2'
+);
+
+SELECT *
+FROM service_history;
